16e64dc3de56188ca1e0b2ac146ffb39c8bc277d
Elias Junno <junnoelias@gmail.com>
2023-11-13 21:20:15 +0200

    assignment 10

---
+++ viikko2/nhl-reader/src/index.py
@@ -0,0 +1,26 @@
+import requests
+from player import Player
+print("asdasd")
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    print(response)
+
+    players = []
+
+    for player_dict in response:
+        if player_dict['nationality'] == 'FIN':
+            player = Player(player_dict)
+            players.append(player)
+
+    print("Oliot:")
+
+    for player in players:
+        print(player)
+
+# team, assists, goals
+if __name__ == '__main__':
+    main()
